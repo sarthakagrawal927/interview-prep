974bb0bca484abc16333f04b9f8f08cbb6de4c0f
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-04-25 22:34:34 +0530

    removed unnecessary file

--- cloneRepo.sh
+++
@@ -1,5 +0,0 @@
-CNTX={users|orgs}; NAME={username|orgname}; PAGE=1
-curl "https://api.github.com/$CNTX/$NAME/repos?page=$PAGE&per_page=100" |
-  grep -e 'git_url*' |
-  cut -d \" -f 4 |
-  xargs -L1 git clone
